ca3777939499c6ca0548951fd8962b8034674fd9
Pavlo <kachurovskiy.p@gmail.com>
2023-03-09 10:39:59 +0100

    Add player move function

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ let getRandom = () => {
 	return Math.floor(Math.random() * 3 + 1);
 }
 
-let getComputerMove = () => {
-	switch(getRandom()) {
+let getMoveByNumber = (moveNumber) => {
+	switch(moveNumber) {
 		case 1: return 'kamień';
 		case 2: return 'papier';
 		case 3: return 'nożyce';
@@ -21,6 +21,18 @@ let getComputerMove = () => {
 	}
 }
 
+let getComputerMove = () => {
+	return getMoveByNumber(getRandom());
+}
+
+let getPlayerMove = () => {
+	let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.') * 1;
+
+	return getMoveByNumber(playerInput);
+}
+
+let playerMove = getPlayerMove();
 let computerMove = getComputerMove();
 
+printMessage('Twój ruch to: ' + playerMove);
 printMessage('Mój ruch to: ' + computerMove);
